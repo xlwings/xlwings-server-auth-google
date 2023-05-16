2f9665fbdfe556e5621344ddcf8ec0ccee411d44
Felix Zumstein <fzumstein@gmail.com>
2023-05-16 11:41:35 +0200

    use Book as context manager

--- app/api/myspreadsheet.py
+++ app/api/myspreadsheet.py
@@ -15,28 +15,28 @@ router = APIRouter(
 @router.post("/hello1")
 async def hello1(data: dict = Body):
     """This endpoint is protected by the router"""
-    book = xw.Book(json=data)
-    sheet = book.sheets[0]
-    cell = sheet["A1"]
-    if cell.value == "Hello xlwings!":
-        cell.value = "Bye xlwings!"
-    else:
-        cell.value = "Hello xlwings!"
-    return book.json()
+    with xw.Book(json=data) as book:
+        sheet = book.sheets[0]
+        cell = sheet["A1"]
+        if cell.value == "Hello xlwings!":
+            cell.value = "Bye xlwings!"
+        else:
+            cell.value = "Hello xlwings!"
+        return book.json()
 
 
 @router.post("/hello2")
 async def hello2(data: dict = Body, current_user: User = Security(authorize)):
     """This is how you access the user object"""
-    book = xw.Book(json=data)
-    sheet = book.sheets[0]
-    cell = sheet["A2"]
-    user_name = current_user.email.split("@")[0]
-    if cell.value == f"Hello {user_name}!":
-        cell.value = f"Bye {user_name}!"
-    else:
-        cell.value = f"Hello {user_name}!"
-    return book.json()
+    with xw.Book(json=data) as book:
+        sheet = book.sheets[0]
+        cell = sheet["A2"]
+        user_name = current_user.email.split("@")[0]
+        if cell.value == f"Hello {user_name}!":
+            cell.value = f"Bye {user_name}!"
+        else:
+            cell.value = f"Hello {user_name}!"
+        return book.json()
 
 
 @router.post("/hello3")
@@ -48,12 +48,12 @@ async def hello3(
     require the user to be an admin. The `auth` module will enforce this by checking if
     the user is in the admin group.
     """
-    book = xw.Book(json=data)
-    sheet = book.sheets[0]
-    cell = sheet["A3"]
-    user_name = current_user.email.split("@")[0]
-    if cell.value == f"Hello {user_name}, you are an admin!":
-        cell.value = f"Bye {user_name}, you are an admin!"
-    else:
-        cell.value = f"Hello {user_name}, you are an admin!"
-    return book.json()
+    with xw.Book(json=data) as book:
+        sheet = book.sheets[0]
+        cell = sheet["A3"]
+        user_name = current_user.email.split("@")[0]
+        if cell.value == f"Hello {user_name}, you are an admin!":
+            cell.value = f"Bye {user_name}, you are an admin!"
+        else:
+            cell.value = f"Hello {user_name}, you are an admin!"
+        return book.json()
